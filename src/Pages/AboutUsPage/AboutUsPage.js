a1f0dd516dfacbd2e00318df6452240fcea4454c
MM-Maketela <maketelamafa2@gmail.com>
2022-10-26 19:53:14 +0200

    updated about us section

--- src/Pages/AboutUsPage/AboutUsPage.js
+++ src/Pages/AboutUsPage/AboutUsPage.js
@@ -1,28 +1,40 @@
 import background1 from '../../Components/drawable/AboutUsBG.jpg';
 import classes from './AboutUsPage.module.css';
 import Section2 from './AboutUsSection2';
+import Section3 from './AboutUsSection3';
+import Section4 from './AboutUsSection4';
+
 const AboutUsPage = () => {
     return (
       <>
-      <div className={classes.AboutUsPage} 
-      style={{ backgroundImage: `url(${background1})` }}>
+      <div className={classes.backgroundForAll}>
+        <div className={classes.AboutUsPage} style={{ backgroundImage: `url(${background1})` }}>
           
-            
-            <span className={classes.text}> About us</span>
-      </div>
-        
-      
-      <div className={classes.section_2}>
-        <div
-        className={classes.alignment}
-        >
-           <Section2/>
+              <span className={classes.text}> About us</span>
         </div>
+          
         
-      </div>
-      
-            
+        <div className={classes.section_2}>
+
+          <div className={classes.alignment}>
+            <Section2/>
+          </div>
+        </div>
+
+        <div className={classes.section_2}>
+          
+            <Section3/>
+        </div>
+
+        <div className={classes.section_2}>
+          
+            <Section4/>
+        </div>
+
+        </div>
+
       </>
+
     );
   };
   
---
+++ src/Pages/AboutUsPage/AboutUsSection4.js
@@ -0,0 +1,11 @@
+import classes from './AboutUsPage.module.css';
+const AboutUsSection4 = ()=>{
+    return (
+        <>
+        <div >
+
+        </div>
+        
+      </>)
+}
+export default AboutUsSection4;
