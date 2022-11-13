b6860e45dd677ae608e5f2d1fd3b77f1a4772c94
MM-Maketela <maketelamafa2@gmail.com>
2022-11-13 20:06:00 +0200

    background images changed in contact us and about us pages

--- src/Pages/AboutUsPage/AboutUsSection1.js
+++ src/Pages/AboutUsPage/AboutUsSection1.js
@@ -1,9 +1,10 @@
 import classes from './AboutUsPage.module.css';
-import img1 from '../../Components/drawable/AboutUsBG.jpg';
+
+import img2 from '../../Components/drawable/img1.jpg';
 function AboutUsSection1() {
   return (
     <div className={classes.section1Parent}>
-        <img src={img1}  alt="section 1 image"  className={classes.section1}/>
+        <img src={img2}  alt="section 1 image"  className={classes.section1}/>
         <p className={classes.aboutUsText}>About us</p>
 
     </div>
--- src/Pages/ContactUs/ContactUsSection1.js
+++ src/Pages/ContactUs/ContactUsSection1.js
@@ -1,10 +1,10 @@
 import classes from '../AboutUsPage/AboutUsPage.module.css';
-import img1 from '../../Components/drawable/AboutUsBG.jpg';
+import img2 from '../../Components/drawable/img1.jpg';
 function ContactUsSection1() {
   return (
     <>
     <div className={classes.section1Parent}>
-        <img src={img1}  alt="section 1 image"  className={classes.section1}/>
+        <img src={img2}  alt="section 1 image"  className={classes.section1}/>
         <p className={classes.aboutUsText}>Contact us</p>
     </div>
 
