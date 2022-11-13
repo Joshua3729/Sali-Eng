fe2e5da40a9d2567f1d5070429d6562f3e25bba3
MM-Maketela <maketelamafa2@gmail.com>
2022-11-13 07:23:22 +0200

    contact us updated

--- src/Pages/AboutUsPage/AboutUsSection4.js
+++ src/Pages/AboutUsPage/AboutUsSection4.js
@@ -8,8 +8,8 @@ const AboutUsSection4 = ()=>{
         <>
         <div className={classes.section4container}>
           <div  className={classes.text_s4_p2}>
-            <h1 className={classes.message}>Specialists on innovative solutions</h1>
-            <p className={classes.paragraph}> If you’re thinking that filler text seems pretty boring and uncontroversial
+            <div className={classes.message}>Specialists on innovative solutions</div>
+            <div className={classes.paragraph}> If you’re thinking that filler text seems pretty boring and uncontroversial
               , you’d be wrong. Surprisingly, there is a very vocal faction of the design community
                that wants to see filler text banished to the original sources from whence it came.
                 Perhaps not surprisingly, in an era of endless quibbling, there is an equally vocal 
@@ -20,11 +20,8 @@ const AboutUsSection4 = ()=>{
                 just slow down the design process. Design first, with real content in mind (of course!),
                 but don’t drop in the real content until the design is well on its way. Using
                 filler text avoids the inevitable argumentation that accompanies the use of real 
-                content in the design process. Those opposed to using filler text of any
-                sort counter by saying: The ultimate purpose of any digital product, whether a
-                website, app, or HTML email, is to showcase real content, not to showcase great
-                design. You can’t get a true sense for how your content plays with your
-                design unless you use the real thing!C/O <pre><a href="https://placeholder.com/text/">placeholder.com</a></pre></p>
+                content in the design process.
+                </div>
           </div>
          
             <div className={classes.containerOfAnimatedImages}>
