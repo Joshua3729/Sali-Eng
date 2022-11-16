b7888b739ad4d96f58050ad23fc8ab0fbc4cd245
MM-Maketela <maketelamafa2@gmail.com>
2022-11-16 15:25:29 +0200

    some changes in projects

--- src/Pages/Projects/ProjectsSection2.js
+++ src/Pages/Projects/ProjectsSection2.js
@@ -8,6 +8,40 @@ import img5 from "../../Components/drawable/img4.jpg";
 import img6 from "../../Components/drawable/img5.jpg";
 import img7 from "../../Components/drawable/img6.jpg";
 import img8 from "../../Components/drawable/img7.jpg";
+
+// let slideIndex = 1;
+// showDivs(slideIndex);
+
+// const showDivs = (n) => {
+
+//   let i;
+//   let x = document.getElementsByClassName("mySlides");
+//   let dots = document.getElementsByClassName("demo");
+//   if (n > x.length) {
+//     slideIndex = 1;
+//   }
+//   if (n < 1) {
+//     slideIndex = x.length;
+//   }
+//   for (i = 0; i < x.length; i++) {
+//     x[i].style.display = "none";
+//   }
+//   for (i = 0; i < dots.length; i++) {
+//     dots[i].className = dots[i].className.replace(" w3-white", "");
+//   }
+//   x[slideIndex - 1].style.display = "block";
+//   dots[slideIndex - 1].className += " w3-white";
+  
+// }
+
+// const currentDiv = (n) => {
+//   showDivs((slideIndex = n));
+// }
+// const plusDivs = (n) => {
+//   showDivs((slideIndex += n));
+// }
+
+
 const ProjectsSection2 = () => {
   return (
     <>
