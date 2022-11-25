306a7704c394e8de8ea73099a608678d2d7c2ff7
Joshua <3729344@myuwc.ac.za>
2022-11-25 22:51:50 +0200

    feat: refactored what we do section (wp)

--- src/Pages/WhatWeDo/WhatWeDo.js
+++ src/Pages/WhatWeDo/WhatWeDo.js
@@ -1,30 +1,30 @@
-import storage2 from '../../Assets/Images/storage 3.jpeg'
+import storage2 from "../../Assets/Images/storage3.jpeg";
+import image3 from "../../Assets/Images/desk.jpeg";
 
-import classes  from "../WhatWeDo/WhatWeDo.module.css"
-import  Section2 from "../WhatWeDo/section2wwd";
-import  Section3 from "../WhatWeDo/section3wwd";
+import classes from "../WhatWeDo/WhatWeDo.module.css";
+import Section2 from "../WhatWeDo/section2wwd";
+import Section3 from "../WhatWeDo/section3wwd";
 const WhatWeDo = () => {
-  
   return (
-    <>
-      <div className={classes.mySection}>
-      <div className={classes.section1} >
-      <div className={classes.whatwedo}>What We Do </div>
-        <img src={storage2} alt='section 3 image' className={classes.animate}
-        />
-         <p className={classes.para}></p>
+    <div className={classes.mySection}>
+      <div className={classes.whatwedo_hero}>
+        <div className={classes.whatwedo}>What We Do </div>
+        <div
+          className={classes.section_img_wrapper}
+          style={{ backgroundImage: `url(${storage2})` }}
+        ></div>
+
+        <p className={classes.para}></p>
       </div>
-     
+
       <div className={classes.section2wwd}>
-        <Section2/>
-      </div> 
+        <Section2 />
+      </div>
 
       <div className={classes.section3wwd}>
         <Section3 />
-        </div>
-
-        </div>
-    </>
+      </div>
+    </div>
   );
 };
 
--- src/Pages/WhatWeDo/section2wwd.js
+++ src/Pages/WhatWeDo/section2wwd.js
@@ -2,47 +2,43 @@ import classes from "../WhatWeDo/WhatWeDo.module.css";
 import image1 from "../../Assets/Images/store.jpeg";
 import image2 from "../../Assets/Images/clothingrack1.jpeg";
 import image3 from "../../Assets/Images/desk.jpeg";
+import Card from "./components/Card";
 
 const section2wwd = () => {
   return (
-    <>
-      <div className={classes.retail.space}>
+    <div className={classes.section_2}>
+      <div className={classes.retail_space}>
         <h4 className={classes.H4div}>Innovating The Retail Space</h4>
-         <h1  className={classes.H1div}>We provide high quality services</h1>
-        <p  className={classes.Pdiv}>
+        <h1 className={classes.H1div}>We provide high quality services</h1>
+        <p className={classes.Pdiv}>
           Our nealry 5200 square metre production facility with state of the art
           machinery.
         </p>
       </div>
-      <div className={classes.wwdSection}>
-        <div className={classes.design}>
-          <img src={image1} alt="section 3 image" className={classes.img1} />
-          <h2>Design</h2>
-          <p>
-            Our Design team cover 2D Design / DTP / 3D design and Rendering as
-            well as Mechanical Design and Engineerin
-          </p>
-        </div>
-
-        <div className={classes.plastic}>
-          <img src={image2} alt="section 3 image" className={classes.img2} />
-          <h2>Plastic Manufactoring</h2>
-          <p>
-            Our Design team cover 2D Design / DTP / 3D design and Rendering as
-            well as Mechanical Design and Engineerin
-          </p>
-        </div>
-
-        <div className={classes.manufactoring}>
-          <img src={image3} alt="section 3 image" className={classes.img3} />
-          <h2>Manufactoring</h2>
-          <p>
-            Our Design team cover 2D Design / DTP / 3D design and Rendering as
-            well as Mechanical Design and Engineerin
-          </p>
-        </div>
+      <div className={classes.cards_gridWrapper}>
+        <Card
+          img={image1}
+          header_text={"Plastic Manufactoring"}
+          card_text={
+            "Our Design team cover 2D Design / DTP / 3D design and Rendering as well as Mechanical Design and Engineerin"
+          }
+        />
+        <Card
+          img={image2}
+          header_text={"Plastic Manufactoring"}
+          card_text={
+            " Our Design team cover 2D Design / DTP / 3D design and Rendering as well as Mechanical Design and Engineerin"
+          }
+        />
+        <Card
+          img={image3}
+          header_text={"Manufactoring"}
+          card_text={
+            "Our Design team cover 2D Design / DTP / 3D design and Rendering as well as Mechanical Design and Engineerin"
+          }
+        />
       </div>
-    </>
+    </div>
   );
 };
 
