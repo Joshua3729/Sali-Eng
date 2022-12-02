c1823f8eba199f32bcc88b468ad5953fbeb5bf79
MM-Maketela <maketelamafa2@gmail.com>
2022-12-02 08:25:47 +0200

    updates

--- src/Pages/Projects/Projects.js
+++ src/Pages/Projects/Projects.js
@@ -1,12 +1,12 @@
 import classes from "../AboutUsPage/AboutUsPage.module.css";
 import ProjectsSection1 from "./ProjectsSection1";
 import ProjectsSection2 from "./ProjectsSection2";
-import bgcolor from '../AboutUsPage/AboutUsPage.module.css';
+import bgColor from '../AboutUsPage/AboutUsPage.module.css';
 
 const Projects = () => {
   return (
     <>
-      <div className={bgcolor.backgroundForAll}>
+      <div className={bgColor.backgroundForAll}>
         <div className={classes.section1_}>
           <ProjectsSection1 />
         </div>
--- src/Pages/Projects/ProjectsSection2.js
+++ src/Pages/Projects/ProjectsSection2.js
@@ -8,7 +8,8 @@ import img5 from "../../Components/drawable/img4.jpg";
 import img6 from "../../Components/drawable/img5.jpg";
 import img7 from "../../Components/drawable/img6.jpg";
 import img8 from "../../Components/drawable/img7.jpg";
-import ImageSlider from './ImageSlider/ImageSlider';
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
 
 
 {/* <div className={classes.projectList}>
@@ -24,15 +25,22 @@ import ImageSlider from './ImageSlider/ImageSlider';
 </div>; */}
 
 const slides = [
-  { image:  img1 , title: "mytitle" },
-  { image:  img2 , title: "mytitle" },
-  { image:  img3 , title: "mytitle" },
-  { image:  img4 , title: "mytitle" },
-  { image: img5 , title: "mytitle" },
-  { image:  img6 , title: "mytitle" },
-  { image:  img7 , title: "mytitle" },
-  { image:  img8 , title: "mytitle" }
+  { key: 0, image: img1, title: "mytitle" },
+  { key: 1, image: img2, title: "mytitle" },
+  { key: 2, image: img3, title: "mytitle" },
+  { key: 3, image: img4, title: "mytitle" },
+  { key: 4, image: img5, title: "mytitle" },
+  { key: 5, image: img6, title: "mytitle" },
+  { key: 6, image: img7, title: "mytitle" },
+  { key: 7, image: img8, title: "mytitle" },
 ];
+const handleDragStart = (e) => e.preventDefault();
+const items = [
+  <img src={img1} onDragStart={handleDragStart} role="presentation" />,
+  <img src={img2} onDragStart={handleDragStart} role="presentation" />,
+  <img src={img3} onDragStart={handleDragStart} role="presentation" />,
+];
+
 const ProjectsSection2 = () => {
   return (
     <>
@@ -43,10 +51,21 @@ const ProjectsSection2 = () => {
       </div>
 
       <div>
-        <ImageSlider slides={slides} />
+        {/* <AliceCarousel>
+          {slides.map((image) => (
+            <div key={image.key} className={classes.text1}>
+              {<img src={image.image} className={classes.listItem} />}
+            </div>
+          ))}
+        </AliceCarousel> */}
+
+        <AliceCarousel
+          onDragStart={handleDragStart}
+          role="presentation"
+          items={items}
+        />
       </div>
     </>
   );
 }
-
 export default ProjectsSection2
