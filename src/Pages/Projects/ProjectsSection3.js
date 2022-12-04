541bc6d7042721ae0fe7c7794ed1019e66469ccd
MM-Maketela <maketelamafa2@gmail.com>
2022-12-04 19:42:19 +0200

    updates

--- src/Pages/Projects/ProjectsSection3.js
+++ src/Pages/Projects/ProjectsSection3.js
@@ -39,77 +39,98 @@ export default class ProjectsSection3 extends Component {
   render() {
     return (
       <>
-      <div className={classes.projectsSection3}>
+        <div className={classes.projectsSection3}>
           <div className={classes.section}>
-            
-            
-        <div className={classes.message}>
-                        message
-        </div>
-        
-        <div className={classes.form}>
-        <form className={classes.contactUsForm}>
-        <div className={classes.formTitle}>Send us a message</div>
-        <div className={classes.formSubtitle}>
-          Contact us today and our friendly support team will reach out as soon
-          as possible.
-        </div>
+            <div className={classes.message}>
+              <div className={classes.deals}>
+                <div className={classes.exclusive}>Exclusive Deals </div>
 
-        <div className={classes.containerWithinForm}>
-          <div className={classes.formContent}>
-            <label className={classes.formSubtitle}>Full name</label>
-            <input
-              type="text"
-              value={this.state.userName}
-              className={classes.input}
-              placeholder="David Parker"
-              onChange={this.handleUserName}
-            ></input>
+                <div>
+                  Find out more about how we can help you with your branded.
+                </div>
+              </div>
+
+              <div className={classes.innerGrid}>
+                <div className={classes.subjectLine}>Looking After..! The Details And The Planet </div>
+                <div className={classes.storyLine}>
+                  Rochelle Retail Solutions is able to provide a eco-friendly
+                  solution and advice on the Green alternatives for your
+                  Projects. 85% of all Waste raw material are Recycled or
+                  Re-worked after each production process.
+                </div>
+                <div>
+                  <label  className={classes.moreInfoButton}>
+                    email  us for information.
+                  </label>
+                </div>
+
+              </div>
+            </div>
+
+            <div className={classes.form}>
+              <form className={classes.contactUsForm}>
+                <div className={classes.formTitle}>Get In Touch </div>
+                <div className={classes.formSubtitle}>
+                  Contact us today and our friendly support team will reach out
+                  as soon as possible.
+                </div>
+
+                <div className={classes.containerWithinForm}>
+                  <div className={classes.formContent}>
+                    <label className={classes.formSubtitle}>Full name</label>
+                    <input
+                      type="text"
+                      value={this.state.userName}
+                      className={classes.input}
+                      placeholder="David Parker"
+                      onChange={this.handleUserName}
+                    ></input>
+                  </div>
+
+                  <div className={classes.formContent}>
+                    <label className={classes.formSubtitle}>
+                      Email address
+                    </label>
+                    <input
+                      type="text"
+                      value={this.state.userEmail}
+                      className={classes.input}
+                      placeholder="myEmail@gmail.com"
+                      onChange={this.handleUserEmail}
+                    ></input>
+                  </div>
+
+                  <div className={classes.formContent}>
+                    <label className={classes.formSubtitle}>Subject</label>
+                    <input
+                      type="text"
+                      value={this.state.userSubject}
+                      className={classes.input}
+                      placeholder="Subject"
+                      onChange={this.handleUserSubject}
+                    ></input>
+                  </div>
+
+                  <div className={classes.paragraphInForm}>
+                    <label className={classes.formSubtitle}>
+                      Write your message below
+                    </label>
+
+                    <textarea
+                      value={this.state.userMessage}
+                      onChange={this.handleUserMessage}
+                    ></textarea>
+
+                    <button className={classes.buttonSubmit}>
+                      Submit form
+                    </button>
+                  </div>
+                </div>
+              </form>
+            </div>
           </div>
-
-          <div className={classes.formContent}>
-            <label className={classes.formSubtitle}>Email address</label>
-            <input
-              type="text"
-              value={this.state.userEmail}
-              className={classes.input}
-              placeholder="myEmail@gmail.com"
-              onChange={this.handleUserEmail}
-            ></input>
-          </div>
-
-          <div className={classes.formContent}>
-            <label className={classes.formSubtitle}>Subject</label>
-            <input
-              type="text"
-              value={this.state.userSubject}
-              className={classes.input}
-              placeholder="Subject"
-              onChange={this.handleUserSubject}
-            ></input>
-          </div>
-
-          <div className={classes.paragraphInForm}>
-            <label className={classes.formSubtitle}>
-              Write your message below
-            </label>
-
-            <textarea
-              value={this.state.userMessage}
-              onChange={this.handleUserMessage}
-            >
-              
-            </textarea>
-
-
-            <button className={classes.buttonSubmit}>Submit form</button>
-          </div>
-        </div>
-      </form>
-      </div>
-      </div>
         </div>
       </>
-    )
+    );
   }
 }
