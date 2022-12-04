b63ac237c6537299d8c65494fdf1f48ed09d2972
MM-Maketela <maketelamafa2@gmail.com>
2022-12-04 21:41:30 +0200

    updates

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Footer.css';
+const Footer = () => {
+  return (
+    <div className="Footer">
+      <div className="innerDiv">
+        Footer
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
