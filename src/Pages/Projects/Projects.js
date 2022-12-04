141a0158e114076c3f4d80c54466461889368c76
MM-Maketela <maketelamafa2@gmail.com>
2022-12-04 16:58:48 +0200

    updates

--- src/Pages/Projects/Projects.js
+++ src/Pages/Projects/Projects.js
@@ -1,6 +1,7 @@
 import classes from "../AboutUsPage/AboutUsPage.module.css";
 import ProjectsSection1 from "./ProjectsSection1";
 import ProjectsSection2 from "./ProjectsSection2";
+import ProjectsSection3 from "./ProjectsSection3";
 import bgColor from '../AboutUsPage/AboutUsPage.module.css';
 
 const Projects = () => {
@@ -13,6 +14,9 @@ const Projects = () => {
 
         <div className={classes.section}>
           <ProjectsSection2 />
+        </div>
+        <div>
+          <ProjectsSection3 />
         </div>
       </div>
     </>
---
+++ src/Pages/Projects/ProjectsSection3.js
@@ -0,0 +1,115 @@
+import React, { Component } from 'react'
+// import classes from '../ContactUs/ContactUs.module.css';
+import classes from './Projects.module.css';
+export default class ProjectsSection3 extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+          userName: '',
+          userEmail: '',
+          userSubject: '',
+          userMessage: ''
+        }
+      }
+    
+
+      handleUserName = event => {
+        this.setState({
+          userName : event.target.value,
+        })
+      }
+    
+      handleUserEmail = event => {
+        this.setState({
+            userEmail: event.target.value
+        })
+      }
+    
+       handleUserSubject = event => {
+         this.setState({
+           userSubject: event.target.value
+         })
+       }
+    
+       handleUserMessage = event => {
+         this.setState({
+           userMessage : event.target.value
+         })
+       }
+  render() {
+    return (
+      <>
+      <div className={classes.projectsSection3}>
+          <div className={classes.section}>
+            
+            
+        <div className={classes.message}>
+                        message
+        </div>
+        
+        <div className={classes.form}>
+        <form className={classes.contactUsForm}>
+        <div className={classes.formTitle}>Send us a message</div>
+        <div className={classes.formSubtitle}>
+          Contact us today and our friendly support team will reach out as soon
+          as possible.
+        </div>
+
+        <div className={classes.containerWithinForm}>
+          <div className={classes.formContent}>
+            <label className={classes.formSubtitle}>Full name</label>
+            <input
+              type="text"
+              value={this.state.userName}
+              className={classes.input}
+              placeholder="David Parker"
+              onChange={this.handleUserName}
+            ></input>
+          </div>
+
+          <div className={classes.formContent}>
+            <label className={classes.formSubtitle}>Email address</label>
+            <input
+              type="text"
+              value={this.state.userEmail}
+              className={classes.input}
+              placeholder="myEmail@gmail.com"
+              onChange={this.handleUserEmail}
+            ></input>
+          </div>
+
+          <div className={classes.formContent}>
+            <label className={classes.formSubtitle}>Subject</label>
+            <input
+              type="text"
+              value={this.state.userSubject}
+              className={classes.input}
+              placeholder="Subject"
+              onChange={this.handleUserSubject}
+            ></input>
+          </div>
+
+          <div className={classes.paragraphInForm}>
+            <label className={classes.formSubtitle}>
+              Write your message below
+            </label>
+
+            <textarea
+              value={this.state.userMessage}
+              onChange={this.handleUserMessage}
+            >
+              
+            </textarea>
+
+
+            <button className={classes.buttonSubmit}>Submit form</button>
+          </div>
+        </div>
+      </form>
+      </div>
+      </div>
+        </div>
+      </>
+    )
+  }
+}
