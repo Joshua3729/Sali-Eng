8f52badb61970fc8b08df71b47cc1ef99218e755
Siphemkhosi <siphemkhosi7@gmail.com>
2022-12-06 07:03:56 +0200

    solved merge

--- src/Pages/WhatWeDo/components/Card.js
+++ src/Pages/WhatWeDo/components/Card.js
@@ -1,20 +1,20 @@
-import classes from "./Card.module.css";
+// import classes from "./Card.module.css";
 
-const Card = ({ img }) => {
-  return (
-    <div className={classes.Card}>
-      <div className={classes.img_wrapper}>
-        <img src={img} alt="section 3 image" className={classes.card_img} />
-      </div>
-      <div className={classes.text_wrapper}>
-        <h2>Design</h2>
-        <p>
-          Our Design team cover 2D Design / DTP / 3D design and Rendering as
-          well as Mechanical Design and Engineerin
-        </p>
-      </div>
-    </div>
-  );
-};
+// const Card = ({ img }) => {
+//   return (
+//     <div className={classes.Card}>
+//       <div className={classes.img_wrapper}>
+//         <img src={img} alt="section 3 image" className={classes.card_img} />
+//       </div>
+//       <div className={classes.text_wrapper}>
+//         <h2>Design</h2>
+//         <p>
+//           Our Design team cover 2D Design / DTP / 3D design and Rendering as
+//           well as Mechanical Design and Engineerin
+//         </p>
+//       </div>
+//     </div>
+//   );
+// };
 
-export default Card;
+// export default Card;
