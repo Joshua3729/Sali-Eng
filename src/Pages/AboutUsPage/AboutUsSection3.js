52c0ef0983896093b6cf9259940a5c0b9f90db74
MM-Maketela <maketelamafa2@gmail.com>
2022-12-07 15:02:19 +0200

    about us updates

--- src/Pages/AboutUsPage/AboutUsSection3.js
+++ src/Pages/AboutUsPage/AboutUsSection3.js
@@ -9,6 +9,7 @@ import img5 from '../../Components/drawable/img4.jpg';
 import img6 from '../../Components/drawable/img5.jpg';
 import img7 from '../../Components/drawable/img6.jpg';
 import img8 from '../../Components/drawable/img7.jpg';
+import CountUp from "react-countup";
 
 
 
@@ -16,43 +17,41 @@ import img8 from '../../Components/drawable/img7.jpg';
 const AboutUsSection3 = ()=>{
 
     return (
-        <>
-        
-
-        <div  className={classes.topContainer}> 
-                <div>
-                        <h1 className={classes.count}>+00</h1>
-                        <h1 className={classes.supportingText} >Team members</h1>
-                </div>
-                <div > 
-                        <h1 className={classes.count}>+00</h1>
-                        <h1 className={classes.supportingText}>Yearly Products produced</h1>
-                </div>
+      <>
+        <div className={classes.topContainer}>
+          <div>
+            <h1 className={classes.count}>
+              +<CountUp start={0} end={270} duration={3} />
+            </h1>
+            <h1 className={classes.supportingText}>Team members</h1>
+          </div>
+          <div>
+            <h1 className={classes.count}>
+              +
+              <CountUp start={0} end={1180} duration={3} />
+                 
+            </h1>
+            <h1 className={classes.supportingText}>Yearly Products produced</h1>
+          </div>
         </div>
-        <div  className={classes.gridContainer}>
-        
-                <img src={img1}  alt="grid item"  className={classes.gridItemImg}/>
-            
-                <img src={img2}  alt="grid item"  className={classes.gridItemImg}/>
+        <div className={classes.gridContainer}>
+          <img src={img1} alt="grid item" className={classes.gridItemImg} />
 
-                <img src={img3}  alt="grid item"  className={classes.gridItemImg}/>
-            
-                <img src={img4}  alt="grid item"  className={classes.gridItemImg}/>
-        
-                <img src={img5}  alt="grid item"  className={classes.gridItemImg}/>
-        
-                <img src={img6}  alt="grid item"  className={classes.gridItemImg}/>
+          <img src={img2} alt="grid item" className={classes.gridItemImg} />
 
-                <img src={img7}  alt="grid item"  className={classes.gridItemImg}/>
+          <img src={img3} alt="grid item" className={classes.gridItemImg} />
 
-                <img src={img8}  alt="grid item"  className={classes.gridItemImg}/>
-                
-               
-           
-        
+          <img src={img4} alt="grid item" className={classes.gridItemImg} />
+
+          <img src={img5} alt="grid item" className={classes.gridItemImg} />
+
+          <img src={img6} alt="grid item" className={classes.gridItemImg} />
+
+          <img src={img7} alt="grid item" className={classes.gridItemImg} />
+
+          <img src={img8} alt="grid item" className={classes.gridItemImg} />
         </div>
-        
-        </>
-    )
+      </>
+    );
 }
 export default AboutUsSection3;
