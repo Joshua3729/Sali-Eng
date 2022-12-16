fa743ed90efecfa1ccb22e2adbc8678264726439
Joshua <3729344@myuwc.ac.za>
2022-12-16 17:27:34 +0200

    feat: added hero cards

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -7,6 +7,10 @@ import clothingrack1 from "../../Assets/Images/clothingrack1.jpeg";
 import clothingrack2 from "../../Assets/Images/clothingrack2.jpeg";
 
 import desk from "../../Assets/Images/desk.jpeg";
+import cardPic1 from "../../Assets/Images/cardPic.png";
+import cardPic2 from "../../Assets/Images/cardPic2.png";
+import cardPic3 from "../../Assets/Images/cardPic5.png";
+
 import TextAnimation from "../../Pages/LandingPage/Components/TextAnimation";
 
 const Hero = () => {
@@ -32,14 +36,27 @@ const Hero = () => {
         {/* <h1 className={classes.CTA_Header}>
           Your One Stop Shop For Retail Solutions. Come Work With Us
         </h1> */}
-        <TextAnimation text={"Tommorrow is designed today!"} header={true} />
-        <p className={classes.secondaryHeader}>
-          At ILUMI DESIGN we design metal, plastic and wood work for clients.
-          We also create logos and all aided design art work. We organise
-          manufacturing companies for our clients to get quotation and get their
-          product running.
-        </p>
-        <button className={classes.CTA_btn}>Explore More</button>
+        <div className={classes.CTA_innerWrapper}>
+          <TextAnimation text={"Tommorrow is designed today!"} header={true} />
+          <p className={classes.secondaryHeader}>
+            At ILUMI DESIGN we design metal, plastic and wood work for clients.
+            We also create logos and all aided design art work. We organise
+            manufacturing companies for our clients to get quotation and get
+            their product running.
+          </p>
+          <button className={classes.CTA_btn}>Explore More</button>
+        </div>
+        <div className={classes.cardsWrapper}>
+          <div className={classes.heroCard}>
+            <img src={cardPic1} />
+          </div>
+          <div className={classes.heroCard}>
+            <img src={cardPic2} />
+          </div>
+          <div className={classes.heroCard}>
+            <img src={cardPic3} />
+          </div>
+        </div>
       </div>
       <div className={classes.BgTabs}>
         {images.map((slide, i) => {
