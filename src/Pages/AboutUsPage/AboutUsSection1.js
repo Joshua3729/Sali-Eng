65c28e9446ca5b24cddd93ea7dfa1b8d10cefbed
Joshua <3729344@myuwc.ac.za>
2022-12-20 21:11:47 +0200

    feat: added banner

--- src/Pages/AboutUsPage/AboutUsSection1.js
+++ src/Pages/AboutUsPage/AboutUsSection1.js
@@ -4,12 +4,14 @@ import teamImg from "../../Assets/Images/team_meeting.avif";
 import PageBanner from "./SVGComponent/PageBanner";
 function AboutUsSection1() {
   return (
-    <div
-      className={classes.section1Parent}
-      style={{
-        backgroundImage: ` linear-gradient(to right,  rgba(24, 27, 41, 0.7), rgba(108, 117, 134, 0.6)), url(${teamImg})`,
-      }}
-    >
+    <div className={classes.section1Parent}>
+      <div
+        className={classes.innerWrapper}
+        style={{
+          backgroundImage: ` linear-gradient(to right,  rgba(24, 27, 41, 0.7), rgba(108, 117, 134, 0.6)), url(${teamImg})`,
+        }}
+      ></div>
+
       <PageBanner />
 
       <p className={classes.aboutUsText}>About us</p>
